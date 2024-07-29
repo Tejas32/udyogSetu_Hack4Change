5d926b717f805c786bf026c92305f386ddc8f26f
Lakshya Makode <114764192+lakshya001-AI@users.noreply.github.com>
2024-07-29 22:27:36 +0530

    Update createAccount.jsx

--- Frontend/src/Components/createAccount.jsx
+++ Frontend/src/Components/createAccount.jsx
@@ -25,7 +25,7 @@ function CreateAccountPage() {
     if(name && aadhaarNumber && password){
 
       try {
-        await axios.post("http://localhost:5000/createAccount", {name,aadhaarNumber,password})
+        await axios.post("https://udyog-setu-hack4-change-backend.vercel.app/createAccount", {name,aadhaarNumber,password})
         .then((res)=>{
 
           toast.success('Account Created! Please login', {
